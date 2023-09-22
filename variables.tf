60b3d32e6f6e09c592192347c57eca68e60af97b
jpetrovic13 <jakov.petrovic@kpmg.co.uk>
2023-09-22 10:15:26 +0100

    New changes

--- variables.tf
+++ variables.tf
@@ -1,6 +1,18 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
+variable "aws_access_key" {
+  type = string
+  description = "AWS access key"
+  default = "AKIA2LQ3TBZTKIS7MPMQ"
+  sensitive = true
+}
 
+variable "aws_secret_key" {
+  type = string
+  description = "AWS secret key"
+  default = "nc/y1rbdijXoeEemp9nIla1M0QQbw0f99VQS3Ua9"
+  sensitive = true
+}
 variable "region" {
   description = "The AWS region your resources will be deployed"
 }
